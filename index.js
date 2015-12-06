277a2f36b9a761b23ef380c938cf4a2774e03eb9
Ben Drucker <bvdrucker@gmail.com>
2015-12-06 08:54:38 -0800

    Format code w/ standard

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-'use strict';
+'use strict'
 
-var toArray   = require('stream-to-array');
-var Promise   = require('bluebird');
-var internals = {};
+var toArray = require('stream-to-array')
+var Promise = require('bluebird')
+var internals = {}
 
 internals.readable = function (stream) {
-  var promise = toArray(stream);
+  var promise = toArray(stream)
 
   // Ensure stream is in flowing mode
-  stream.resume();
+  stream.resume()
 
   return promise
     .then(function (parts) {
-      var buffers = [];
-      for (var i = 0, l = parts.length; i < l ; ++i) {
-        var part = parts[i];
-        buffers.push((part instanceof Buffer) ? part : new Buffer(part));
+      var buffers = []
+      for (var i = 0, l = parts.length; i < l; ++i) {
+        var part = parts[i]
+        buffers.push((part instanceof Buffer) ? part : new Buffer(part))
       }
-      return Buffer.concat(buffers);
-    });
-};
+      return Buffer.concat(buffers)
+    })
+}
 
 internals.writable = function (stream) {
   return new Promise(function (resolve, reject) {
-    stream.once('finish', resolve);
-    stream.once('error', reject);
-  });
-};
+    stream.once('finish', resolve)
+    stream.once('error', reject)
+  })
+}
 
 module.exports = function (stream) {
-  var promise;
+  var promise
   if (stream.readable) {
-    promise = internals.readable(stream);
+    promise = internals.readable(stream)
+  } else if (stream.writable) {
+    promise = internals.writable(stream)
+  } else {
+    promise = Promise.resolve()
   }
-  else if (stream.writable) {
-    promise = internals.writable(stream);
-  }
-  else {
-    promise = Promise.resolve();
-  }
-  return promise;
-};
+  return promise
+}
--- test.js
+++ test.js
@@ -1,143 +1,135 @@
-'use strict';
+'use strict'
 
-var Stream          = require('stream');
-var chai            = require('chai');
-var Promise         = require('bluebird');
-var DelayedStream   = require('delayed-stream');
-var fs              = Promise.promisifyAll(require('fs'));
-var rimraf          = Promise.promisify(require('rimraf'));
-var streamToPromise = require('./');
+var Stream = require('stream')
+var chai = require('chai')
+var Promise = require('bluebird')
+var DelayedStream = require('delayed-stream')
+var fs = Promise.promisifyAll(require('fs'))
+var rimraf = Promise.promisify(require('rimraf'))
+var streamToPromise = require('./')
 
-chai.use(require('chai-as-promised'));
+chai.use(require('chai-as-promised'))
 Promise.onPossiblyUnhandledRejection(function (err) {
-  throw err;
-});
+  throw err
+})
 
-var expect = chai.expect;
+var expect = chai.expect
 
-/* globals describe:false, it:false, beforeEach:false */
+/* globals describe:false, it:false, beforeEach:false, before:false, after:false */
 
 describe('stream-to-promise', function () {
-
   describe('Readable streams', function () {
-
-    var readable, promise;
+    var readable, promise
     beforeEach(function () {
-      readable = new Stream.Readable();
-      readable._read = function noop () {};
-      promise = streamToPromise(readable);
-    });
+      readable = new Stream.Readable()
+      readable._read = function noop () {}
+      promise = streamToPromise(readable)
+    })
 
     it('can resolve empty streams', function () {
-      readable.emit('end');
+      readable.emit('end')
       return promise.then(function (buffer) {
-        expect(buffer).to.be.an.instanceOf(Buffer);
-        expect(buffer).to.have.length(0);
-      });
-    });
+        expect(buffer).to.be.an.instanceOf(Buffer)
+        expect(buffer).to.have.length(0)
+      })
+    })
 
     it('resolves stream data', function () {
-      readable.emit('data', new Buffer('foo'));
-      readable.emit('data', new Buffer('bar'));
-      readable.emit('end');
+      readable.emit('data', new Buffer('foo'))
+      readable.emit('data', new Buffer('bar'))
+      readable.emit('end')
       return promise.then(function (buffer) {
-        expect(buffer).to.be.an.instanceOf(Buffer);
-        expect(buffer.toString()).to.equal('foobar');
-      });
-    });
+        expect(buffer).to.be.an.instanceOf(Buffer)
+        expect(buffer.toString()).to.equal('foobar')
+      })
+    })
 
     it('can handle streams of buffers and strings', function () {
-      readable.emit('data', new Buffer('foo'));
-      readable.emit('data', 'bar');
-      readable.emit('end');
+      readable.emit('data', new Buffer('foo'))
+      readable.emit('data', 'bar')
+      readable.emit('end')
       return promise.then(function (buffer) {
-        expect(buffer.toString()).to.equal('foobar');
-      });
-    });
+        expect(buffer.toString()).to.equal('foobar')
+      })
+    })
 
     it('resolves immediately for ended streams', function () {
-      readable.readable = false;
+      readable.readable = false
       return streamToPromise(readable).then(function (result) {
-        expect(result).to.be.undefined;
-      });
-    });
+        expect(result).to.be.undefined
+      })
+    })
 
     it('ensures that streams are flowing (#1)', function () {
-      var delayed = DelayedStream.create(readable);
-      readable.emit('data', new Buffer('foo'));
-      readable.emit('end');
+      var delayed = DelayedStream.create(readable)
+      readable.emit('data', new Buffer('foo'))
+      readable.emit('end')
       return streamToPromise(delayed).then(function (buffer) {
-        expect(buffer.toString()).to.equal('foo');
-      });
-    });
+        expect(buffer.toString()).to.equal('foo')
+      })
+    })
 
     it('rejects on stream errors', function () {
-      var err = new Error();
-      readable.emit('error', err);
-      return expect(promise).to.be.rejectedWith(err);
-    });
-
-  });
+      var err = new Error()
+      readable.emit('error', err)
+      return expect(promise).to.be.rejectedWith(err)
+    })
+  })
 
   describe('Writable streams (#2)', function () {
-
-    var writable, promise;
+    var writable, promise
     beforeEach(function () {
-      writable = new Stream.Writable();
-      writable._read = function noop () {};
-      promise = streamToPromise(writable);
-    });
+      writable = new Stream.Writable()
+      writable._read = function noop () {}
+      promise = streamToPromise(writable)
+    })
 
     it('resolves undefined when the stream finishes', function () {
-      writable.emit('finish');
-      return expect(promise).to.eventually.be.undefined;
-    });
+      writable.emit('finish')
+      return expect(promise).to.eventually.be.undefined
+    })
 
     it('rejects on stream errors', function () {
-      var err = new Error();
-      writable.emit('error', err);
-      return expect(promise).to.be.rejectedWith(err);
-    });
-
-  });
+      var err = new Error()
+      writable.emit('error', err)
+      return expect(promise).to.be.rejectedWith(err)
+    })
+  })
 
   describe('Integration', function () {
-
-    var tmp;
+    var tmp
     before(function () {
-      tmp = __dirname + '/tmp';
-      return fs.mkdirAsync(tmp);
-    });
+      tmp = __dirname + '/tmp'
+      return fs.mkdirAsync(tmp)
+    })
 
     it('can handle an fs read stream', function () {
       return fs.writeFileAsync(tmp + '/read.txt', 'hi there!')
         .then(function () {
-          return streamToPromise(fs.createReadStream(tmp + '/read.txt'));
+          return streamToPromise(fs.createReadStream(tmp + '/read.txt'))
         })
         .then(function (contents) {
-          expect(contents.toString()).to.equal('hi there!');
-        });
-    });
+          expect(contents.toString()).to.equal('hi there!')
+        })
+    })
 
     it('can handle an fs write stream', function () {
-      var stream = fs.createWriteStream(tmp + '/written.txt');
+      var stream = fs.createWriteStream(tmp + '/written.txt')
       process.nextTick(function () {
-        stream.write('written contents');
-        stream.end();
-      });
+        stream.write('written contents')
+        stream.end()
+      })
       return streamToPromise(stream)
         .then(function () {
-          return fs.readFileAsync(tmp + '/written.txt');
+          return fs.readFileAsync(tmp + '/written.txt')
         })
         .then(function (contents) {
-          expect(contents.toString()).to.equal('written contents');
-        });
-    });
+          expect(contents.toString()).to.equal('written contents')
+        })
+    })
 
     after(function () {
-      return rimraf(tmp);
-    });
-
-  });
-
-});
+      return rimraf(tmp)
+    })
+  })
+})
